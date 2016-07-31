eee7c31bd456ef214e3acbd9e51dca7c54d13c79
AnastasiiaK <kranchenko.anastasiia@gmail.com>
2016-07-31 16:51:35 +0300

    Urls

--- src/states/Preload.js
+++ src/states/Preload.js
@@ -2,22 +2,22 @@ import Phaser from 'phaser';
 
 export default class extends Phaser.State {
     preload() {
-        this.game.load.image('bg', '../../assets/bg.png');
-        this.game.load.image('paddle', '../../assets/platform.png');
-        this.game.load.image('brick', '../../assets/brick.png');
-        this.game.load.image('goldBrick', '../../assets/goldBrick.png');
-        this.game.load.image('ball', '../../assets/axe.png');
+        this.game.load.image('bg', 'assets/bg.png');
+        this.game.load.image('paddle', 'assets/platform.png');
+        this.game.load.image('brick', 'assets/brick.png');
+        this.game.load.image('goldBrick', 'assets/goldBrick.png');
+        this.game.load.image('ball', 'assets/axe.png');
 
-        this.game.load.spritesheet('button', '../../assets/Button2.png', 400, 172);
-        this.game.load.spritesheet('background', '../../assets/TileSet.png', 32, 32);
+        this.game.load.spritesheet('button', 'assets/Button2.png', 400, 172);
+        this.game.load.spritesheet('background', 'assets/TileSet.png', 32, 32);
 
-        this.game.load.audio('hit', '../../assets/media/wood_hit.wav');
-        this.game.load.audio('menuTheme', '../../assets/media/menu_theme.ogg');
-        this.game.load.audio('theme', '../../assets/media/base_theme.ogg');
-        this.game.load.audio('win', '../../assets/media/send.ogg');
-        this.game.load.audio('lose', '../../assets/media/lose.ogg');
+        this.game.load.audio('hit', 'assets/media/wood_hit.wav');
+        this.game.load.audio('menuTheme', 'assets/media/menu_theme.ogg');
+        this.game.load.audio('theme', 'assets/media/base_theme.ogg');
+        this.game.load.audio('win', 'assets/media/send.ogg');
+        this.game.load.audio('lose', 'assets/media/lose.ogg');
 
-        this.game.load.bitmapFont('carrier_command', '../../assets/fonts/carrier_command.png', '../../assets/fonts/carrier_command.xml');
+        this.game.load.bitmapFont('carrier_command', 'assets/fonts/carrier_command.png', 'assets/fonts/carrier_command.xml');
     }
 
     create() {
